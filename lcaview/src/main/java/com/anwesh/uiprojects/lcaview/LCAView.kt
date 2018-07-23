ce773e667e5446a2ef741ee148485be4477f593f
Anwesh43 <anweshthecool0@gmail.com>
2018-07-23 08:32:27 +0530

    importing libraries and defining nodes

--- lcaview/src/main/java/com/anwesh/uiprojects/lcaview/LCAView.kt
+++ lcaview/src/main/java/com/anwesh/uiprojects/lcaview/LCAView.kt
@@ -4,3 +4,11 @@ package com.anwesh.uiprojects.lcaview
  * Created by anweshmishra on 23/07/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+
